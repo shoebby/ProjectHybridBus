4f2909a0e243836eb303c4f4d1845316619938f4
shoebby <59800629+shoebby@users.noreply.github.com>
2021-01-12 22:42:51 +0100

    main gameplay scene tweaks
    ADDED:
    - scene transition bones
    - bushalte prefab
    CHANGED:
    - corrected bus position

--- VRbustrip-GH/Assets/Scripts/sceneChanger.cs
+++ VRbustrip-GH/Assets/Scripts/sceneChanger.cs
@@ -7,10 +7,19 @@ public class sceneChanger : MonoBehaviour
 {
     public Animator transition;
     public float transitionTime = 2f;
+    public GameObject levelLoader;
+    public string sceneName;
+
+    private void Start()
+    {
+        levelLoader = GameObject.FindGameObjectWithTag("levelLoader");
+        transition = levelLoader.GetComponentInChildren<Animator>();
+    }
 
     public void SceneChange(string sceneName)
     {
         StartCoroutine(SceneChangeCoroutine(sceneName));
+
     }
 
     IEnumerator SceneChangeCoroutine(string sceneName)
@@ -20,5 +29,13 @@ public class sceneChanger : MonoBehaviour
         yield return new WaitForSeconds(transitionTime);
         
         SceneManager.LoadScene(sceneName);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(SceneChangeCoroutine(sceneName));
+        }
     }
 }
